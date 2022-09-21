b193a011c7d421a3eec1d10a7a14f8346d2aae2c
Saquib <saquib.qureshi@protonshub.in>
2022-09-21 14:03:13 +0530

    added get

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(full_name: "Saquib", full_phone_number: "xyz@yepmail.comaa", password: "Abc@123", email: "xyz@yepmail.comaa", type: "EmailUser" )
